326cb4f4f190143a5fd14cd6e62e049fe5177cbe
mattdavey <matt.davey540@me.com>
2020-01-06 16:59:05 +0000

    added sendFile;

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const router = require('./config/router')
 
 app.use(logger)
 
-app.use('/api', router)
+app.use('/api', router) 
+//use proxy for api requests
+
+app.use('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
+//any get request come deploy, use index.html at bundle dist folder
 
 app.listen(port, () => console.log(`node server running on :${port}`))

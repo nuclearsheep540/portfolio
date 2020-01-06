82e6ceaa5bc3376f13d0af24ce7f02b3a6fc5690
mattdavey <matt.davey540@me.com>
2020-01-06 18:09:07 +0000

    index js;

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const port = process.env.PORT || 4000
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const logger = require('./lib/logger')
 const router = require('./config/router')
+
+import path from 'path'
 
 app.use(logger)
 
 app.use('/api', router) 
 //use proxy for api requests
+
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, './dist')))
 
 app.use('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 //any get request come deploy, use index.html at bundle dist folder

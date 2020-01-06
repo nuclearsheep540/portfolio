2290cb739f5d62a7d0010ba18f286d31a40a8951
mattdavey <matt.davey540@me.com>
2020-01-06 16:55:38 +0000

    removed mongo backend from index

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-const { dbURI } = require('./config/environment')
 const port = process.env.PORT || 4000
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 const logger = require('./lib/logger')
 const router = require('./config/router')
-// const errorHandler = require('./lib/errorHandler')
-mongoose.set('useFindAndModify', false)
-
-
-
-mongoose.connect(dbURI,
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-  () => console.log('Mongo is connected')
-)
-
-app.use(bodyParser.json())
 
 app.use(logger)
 
 app.use('/api', router)
 
-// app.use(errorHandler)
-
-app.get('/*', (req, res) => res.status(404).json({ message: 'Not found' }))
-
 app.listen(port, () => console.log(`node server running on :${port}`))
-
-module.exports = app

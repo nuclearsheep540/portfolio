ea2b60ffe9120981a8c62b9cfa49bcba9b5f15a1
mattdavey <matt.davey540@me.com>
2020-01-08 13:28:37 +0000

    index js

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const port = process.env.PORT || 4000
+const { dbURI, port } = require('./config/environment')
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const logger = require('./lib/logger')
 const router = require('./config/router')
+const errorHandler = require('./lib/errorHandler')
 
-app.use(express.static(__dirname))
 
+mongoose.connect(dbURI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  console.log('mongo is running')
+)
 app.use(bodyParser.json())
+
 app.use(logger)
-app.use('/api', router) 
 
+app.use('/api', router)
 
-app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
-//any get request come deploy, use index.html at bundle dist folder
+app.use(errorHandler)
+
+app.get('/*', (req, res) => req.status(404).json({ message: 'Not found' }))
 
 app.listen(port, () => console.log(`node server running on :${port}`))

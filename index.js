e11ae8602b4b95fc54529b5e83fcf419a143f6b1
mattdavey <matt.davey540@me.com>
2020-01-08 12:48:41 +0000

    nodemon and appget

--- index.js
+++ index.js
@@ -5,16 +5,14 @@ const bodyParser = require('body-parser')
 const logger = require('./lib/logger')
 const router = require('./config/router')
 
-app.use(logger)
-
-app.use('/api', router) 
-//use proxy for api requests
 
 app.use(bodyParser.json())
+app.use(logger)
+app.use('/api', router) 
 
 app.use(express.static('public'))
 
-app.use('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 //any get request come deploy, use index.html at bundle dist folder
 
 app.listen(port, () => console.log(`node server running on :${port}`))

2651521f3a79b3e6a7d9c26ef5c1926f2097ec9c
mattdavey <matt.davey540@me.com>
2020-01-08 12:32:33 +0000

    entry

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 require('dotenv').config()
 
 module.exports = {
-  entry: './src/app.js',
+  entry: 'src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',

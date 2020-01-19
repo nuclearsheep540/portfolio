1435d8320db2761dc1e31e98310eae7ab4874f6b
mattdavey <matt.davey540@me.com>
2020-01-19 18:59:36 +0000

    contact now fixed

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
+const bodyParser = require('body-parser')
+const logger = require('./lib/logger')
+const router = require('./config/router')
 
 app.use(express.static(`${__dirname}/dist`))
+
+app.use(bodyParser.json())
+
+app.use(logger)
+
+app.use('/api', router)
 
 app.use('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
 app.listen(port, () => console.log(`Express is running on port ${port}`))
+
+module.exports = app
